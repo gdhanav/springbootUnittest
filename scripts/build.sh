64e1ebcda5cf785deb336d96faf58e6eb4fb8f02
dhanavenkatesh <dhanavenkatesh.gopal@outware.com.au>
2018-12-30 22:42:07 +1100

    updated build scripts-3

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,14 @@ APP_DIR="springbootUnittest"
 COMPONENT_DIR="springbootUnittest/scripts"
 PAYLOAD_DIR="/home/ec2-user"
 #Build application package
-cd "${APP_DIR}" && ./gradlew clean assemble && cd ..
+ ./gradlew clean assemble
 
 echo "Moving application artefact to payload directory"
 #Setup payload for upload
 PAYLOAD_APP_DIR="${PAYLOAD_DIR}/app"
 mkdir -p $PAYLOAD_APP_DIR
 chown -R ec2-user:ec2-user $PAYLOAD_APP_DIR
-cp -r "${APP_DIR}/"build/libs/*.jar "${PAYLOAD_APP_DIR}"
+cp -r build/libs/*.jar "${PAYLOAD_APP_DIR}"
 
 echo "Moving application scripts to payload directory"
 cp -r "${COMPONENT_DIR}/scripts/"* "${PAYLOAD_DIR}"
